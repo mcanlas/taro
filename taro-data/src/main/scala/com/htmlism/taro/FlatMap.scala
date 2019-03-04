bb83d8c06824494288a5a33afd89c0e87687c50d
Mark Canlas <github@htmlism.com>
2019-03-04 14:46:29 -0500

    flatmap is an apply

--- taro-data/src/main/scala/com/htmlism/taro/FlatMap.scala
+++ taro-data/src/main/scala/com/htmlism/taro/FlatMap.scala
@@ -2,6 +2,6 @@ package com.htmlism.taro
 
 import simulacrum._
 
-@typeclass trait FlatMap[F[_]] {
+@typeclass trait FlatMap[F[_]] extends Apply[F] {
   @op(">>=") def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 }
